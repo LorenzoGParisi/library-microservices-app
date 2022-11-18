a9c438d17f7b68c64115531ccf1a6c593181994d
M4tt3-0 <100215939+M4tt3-0@users.noreply.github.com>
2022-11-18 17:09:58 +0100

    Feat: Created Get Controllers

--- borrowing/Borrowings.WebApi/Borrowings.WebApi/Controllers/BorrowingController.cs
+++ borrowing/Borrowings.WebApi/Borrowings.WebApi/Controllers/BorrowingController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Borrowings.WebApi.Models;
+using Borrowings.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,25 @@ namespace Borrowings.WebApi.Controllers
     [Route("[controller]")]
     public class BorrowingController : ControllerBase
     {
-        public BorrowingController()
+        private readonly BorrowingServices _service;
+        public BorrowingController(BorrowingServices borrowingServices) =>
+            _service = borrowingServices;
+
+        [HttpGet]
+        public async Task<List<Borrowing>> Get() =>
+            await _service.Get();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Borrowing>> Get(int id)
         {
-            
+            var borrowing = await _service.Get(id);
+
+            if (borrowing is null)
+            {
+                return NotFound();
+            }
+
+            return borrowing;
         }
- 
     }
 }
