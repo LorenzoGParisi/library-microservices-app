fac014a29792ef75d9d4a088a0f53dcce180d864
M4tt3-0 <100215939+M4tt3-0@users.noreply.github.com>
2022-11-18 17:18:24 +0100

    Feat: Startup setup

--- borrowing/Borrowings.WebApi/Borrowings.WebApi/Startup.cs
+++ borrowing/Borrowings.WebApi/Borrowings.WebApi/Startup.cs
@@ -1,3 +1,5 @@
+using Borrowings.WebApi.Data;
+using Borrowings.WebApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +28,8 @@ namespace Borrowings.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<BorrowingServices>();
+            services.Configure<BorrowingDbSettings>(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
