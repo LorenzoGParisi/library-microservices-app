9cd50ec6a68801584bc84ff2b5aea3c1128d3e69
M4tt3-0 <100215939+M4tt3-0@users.noreply.github.com>
2022-11-18 16:56:17 +0100

    Feat: Created DbSettings Constructor

---
+++ borrowing/Borrowings.WebApi/Borrowings.WebApi/Services/BorrowingServices.cs
@@ -0,0 +1,29 @@
+﻿using Borrowings.WebApi.Data;
+using Borrowings.WebApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Borrowings.WebApi.Services
+{
+    public class BorrowingServices
+    {
+        private readonly IMongoCollection<Borrowing> _context;
+
+        public BorrowingServices(
+            IOptions<BorrowingDbSettings> borrowingDbSettings)
+        {
+            var mongoClient = new MongoClient(
+                borrowingDbSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                borrowingDbSettings.Value.DatabaseName);
+
+            _context = mongoDatabase.GetCollection<Borrowing>(
+                borrowingDbSettings.Value.BooksCollectionName);
+        }
+    }
+}
