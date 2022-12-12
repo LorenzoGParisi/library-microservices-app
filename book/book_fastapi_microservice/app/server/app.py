2af952d06913ad1e1f400cdb31df8b442f025acf
MrcllBo <marcello.borio@edu.itspiemonte.it>
2022-12-12 15:20:24 +0100

    Fix: fix app.py

--- book/book_fastapi_microservice/app/server/app.py
+++ book/book_fastapi_microservice/app/server/app.py
@@ -1,18 +1,38 @@
+import os
 from fastapi import FastAPI
-from server.routes.book import router as BookRouter
+from app.server.routes.book import router as BookRouter
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.responses import JSONResponse
+import asyncio
+import nest_asyncio
+import pymongo
+import time
+from pymongo import MongoClient
+
 
 app = FastAPI()
 
+
 app.include_router(BookRouter, tags=["Book"], prefix="/book")
+
+from app.server.database import MONGO_DETAILS, clientMotor
+nest_asyncio.apply()
+
+
+async def get_server_info():
+    client = clientMotor
+
+    try:
+        print(await client.server_info())
+        print('connected')
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("Unable to connect to the database")
+        # os._exit(os.EX_OK)
+        # raise SystemExit
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_server_info())
 
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request, exc):
-    print(f"OMG! An HTTP error!: {repr(exc)}")
-    return JSONResponse({"detail": "Endpoint is wrong"}, status_code=exc.status_code)
+    return JSONResponse ({":detail":"Endpoint is wrong"}, status_code=exc.status_code)
 
-# @app.exception_handler(StarletteHTTPException)
-# async def custom_http_exception_handler(request, exc):
-#     print(f"OMG! An HTTP error!: {repr(exc)}")
-#     return JSONResponse({"detail": "Endpoint is wrong"}, status_code=exc.status_code)
